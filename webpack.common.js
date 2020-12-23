df69879c32fc9b502668d9ff2f5c13e53bdf4707
Gabriela Mozer <gabrielamozer1966@gmail.com>
2020-12-23 11:46:45 +0100

    new file

--- webpack.common.js
+++ webpack.common.js
@@ -88,5 +88,11 @@ module.exports = {
       chunks: ["index", "another"],
       filename: "another.html",
     }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/gabi.html",
+      inject: true,
+      chunks: ["index", "another"],
+      filename: "gabi.html",
+    }),
   ],
 };
